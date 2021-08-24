dd80d0b1b14d841a1274e027f3e70ca26893521f
Robert Stein <SteinRobert@users.noreply.github.com>
2021-08-24 14:21:46 +0200

    fix: handle bug when duplicated model names have been used in schema generation

--- src/projects/utils/value_schema.py
+++ src/projects/utils/value_schema.py
@@ -26,14 +26,16 @@ def create_json_value_schema(input):
     return json.dumps(create_schema(content))
 
 
-def create_pydantic_schema(yaml_dict: dict) -> dict:
+def create_pydantic_schema(yaml_dict: dict, model_names: List) -> (dict, List):
     fields = {}
     for key, value in yaml_dict.items():
         type_ = type(value)
         if type_ is dict:
-            sub_fields = create_pydantic_schema(value)
-            model = create_model(key, **sub_fields)
+            sub_fields, model_names = create_pydantic_schema(value, model_names)
+            name = get_model_name(key, model_names)
+            model = create_model(name, **sub_fields)
             fields[key] = (Optional[model], None)
+            model_names.append(name)
         elif type_ is list:
             if len(value):
                 fields[key] = (Optional[List[type(value[0])]], None)
@@ -46,11 +48,21 @@ def create_pydantic_schema(yaml_dict: dict) -> dict:
                     None,
                 ),
             )
-    return fields
+    return fields, model_names
+
+
+def get_model_name(key: str, model_names: List) -> str:
+    """Generates a name for a pydantic model, based on already taken model names."""
+    if key not in model_names:
+        return key
+    counter = 1
+    while key + str(counter) in model_names:
+        counter += 1
+    return key + str(counter)
 
 
 def create_schema_json(yaml_dict):
-    values_schema = create_model("HelmValuesJsonSchema", __base__=BaseModel, **create_pydantic_schema(yaml_dict))
+    values_schema = create_model("HelmValuesJsonSchema", __base__=BaseModel, **create_pydantic_schema(yaml_dict, [])[0])
     return values_schema.schema()
 
 
